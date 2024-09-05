95234f08f3fabeefe657a30e01715bfec0861a24
AbiPetheram <abi.gill97@gmail.com>
2024-09-05 12:39:23 +0100

    feat: implement getBookById in RecordShopController

--- src/main/java/com/northcoders/record_shop/controller/RecordShopController.java
+++ src/main/java/com/northcoders/record_shop/controller/RecordShopController.java
@@ -7,10 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/album")
@@ -23,5 +25,15 @@ public class RecordShopController {
     public ResponseEntity<List<Album>> getAllAlbums(){
         List<Album> albums = recordShopService.getAllAlbums();
         return new ResponseEntity<>(albums, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Album> getAlbumById(@PathVariable Long id){
+        Optional<Album> album = recordShopService.getAlbumById(id);
+        if(album.isEmpty()){
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(album.get(), HttpStatus.OK);
+        }
     }
 }
