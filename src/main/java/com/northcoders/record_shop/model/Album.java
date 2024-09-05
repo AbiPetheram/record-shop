207460330c290cff4f0529d7877a90891b5b0bc5
AbiPetheram <abi.gill97@gmail.com>
2024-09-05 10:33:25 +0100

    feat: add annotations to album fields

--- src/main/java/com/northcoders/record_shop/model/Album.java
+++ src/main/java/com/northcoders/record_shop/model/Album.java
@@ -1,17 +1,20 @@
 package com.northcoders.record_shop.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Album {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false)
     long id;
 
+    @Column
     String artistName;
+
+    @Column
     int releaseYear;
+
+    @Column
     Genre genre;
 }
