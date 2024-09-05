69187cb1f3b2c55bfdf14b936f8829147ab37177
AbiPetheram <abi.gill97@gmail.com>
2024-09-05 11:19:49 +0100

    feat: implement getAllAlbums method in RecordShopServiceImpl

--- src/main/java/com/northcoders/record_shop/service/RecordShopServiceImpl.java
+++ src/main/java/com/northcoders/record_shop/service/RecordShopServiceImpl.java
@@ -1,7 +1,23 @@
 package com.northcoders.record_shop.service;
 
+import com.northcoders.record_shop.model.Album;
+import com.northcoders.record_shop.repository.RecordShopRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
-public class RecordShopServiceImpl {
+public class RecordShopServiceImpl implements RecordShopService{
+
+    @Autowired
+    RecordShopRepository recordShopRepository;
+
+    @Override
+    public List<Album> getAllAlbums() {
+        List<Album> albums = new ArrayList<>();
+        recordShopRepository.findAll().forEach(albums::add);
+        return albums;
+    }
 }
