eb836f1fad134de830ad9ca775330ddb6d89774a
AbiPetheram <abi.gill97@gmail.com>
2024-09-05 10:32:21 +0100

    feat: add fields to album

--- src/main/java/com/northcoders/record_shop/model/Album.java
+++ src/main/java/com/northcoders/record_shop/model/Album.java
@@ -1,4 +1,17 @@
 package com.northcoders.record_shop.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    String artistName;
+    int releaseYear;
+    Genre genre;
 }
