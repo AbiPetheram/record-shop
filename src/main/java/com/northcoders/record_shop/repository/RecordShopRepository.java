fe2b74d9425ddf2ec3cd165f86fdc68f2744a89c
AbiPetheram <abi.gill97@gmail.com>
2024-09-09 16:49:10 +0100

    feat: extend RecordShopRepository with JpaSpecificationExecutor interface

--- src/main/java/com/northcoders/record_shop/repository/RecordShopRepository.java
+++ src/main/java/com/northcoders/record_shop/repository/RecordShopRepository.java
@@ -1,12 +1,13 @@
 package com.northcoders.record_shop.repository;
 
 import com.northcoders.record_shop.model.Album;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface RecordShopRepository extends CrudRepository<Album, Long> {
+public interface RecordShopRepository extends CrudRepository<Album, Long>, JpaSpecificationExecutor<Album> {
     List<Album> findAllAlbumsByArtistName(String artist);
 }
