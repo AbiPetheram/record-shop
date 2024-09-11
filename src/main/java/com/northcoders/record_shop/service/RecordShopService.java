55261d9925f2595e6bc5803f609ed617ca43e1a6
AbiPetheram <abi.gill97@gmail.com>
2024-09-11 10:26:00 +0100

    feat: add parameters to getAllAlbums method in RecordShopService interface

--- src/main/java/com/northcoders/record_shop/service/RecordShopService.java
+++ src/main/java/com/northcoders/record_shop/service/RecordShopService.java
@@ -1,12 +1,13 @@
 package com.northcoders.record_shop.service;
 
 import com.northcoders.record_shop.model.Album;
+import com.northcoders.record_shop.model.Genre;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface RecordShopService {
-    List<Album> getAllAlbums();
+    List<Album> getAllAlbums(String artist, Genre genre, Integer year);
     Optional<Album> getAlbumById(Long id);
     Album insertAlbum(Album album);
     Album updateAlbumById(Album album, Long id);
