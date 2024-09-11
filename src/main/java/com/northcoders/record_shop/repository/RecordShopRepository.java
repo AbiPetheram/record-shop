7e9e7e3a34ed999fc8713c37e3e84c2766fda17f
AbiPetheram <abi.gill97@gmail.com>
2024-09-11 10:27:53 +0100

    feat: remove redundant methods from RecordShopRepository

--- src/main/java/com/northcoders/record_shop/repository/RecordShopRepository.java
+++ src/main/java/com/northcoders/record_shop/repository/RecordShopRepository.java
@@ -9,7 +9,4 @@ import java.util.List;
 
 @Repository
 public interface RecordShopRepository extends CrudRepository<Album, Long>, JpaSpecificationExecutor<Album> {
-    List<Album> findAllAlbumsByArtistName(String artist);
-//    List<Album> findAllAlbumsByGenre(String genre);
-//    List<Album> findAllAlbumsByReleaseYear(String year);
 }
